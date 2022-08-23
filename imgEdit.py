f6e9e9207be77932280aaab69dc2863c61c5fd48
xtremeevan <80179007+xtremeevan@users.noreply.github.com>
2022-08-23 17:08:24 -0500

    First commit

---
+++ imgEdit.py
@@ -0,0 +1,20 @@
+from importlib.resources import path
+from math import factorial
+from PIL import Image, ImageEnhance, ImageFilter
+import os
+
+path = './img'
+pathOut = '/editedImg'
+
+for filename in os.listdir(path): # opens images in folder
+   img = Image.open(f"{path}/{filename}")
+
+   edit = img.filter(ImageFilter.SHARPEN).convert('L')
+
+   factor = 1.5
+   enhancer = ImageEnhance.Contrast(edit)
+   edit = enhancer.enhance(factor)
+
+   clean_name = os.path.splitext(filename)[0]
+
+   edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
